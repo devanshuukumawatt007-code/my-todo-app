8d48420b32d8e0dcd45fc2776c396ab209a811cf
devanshuukumawatt007-code <devanshuukumawatt007@gmail.com>
2026-02-13 22:37:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("taskList");
 
+addBtn.addEventListener("click", addTask);
+
+function addTask() {
+
+  if (input.value.trim() === "") {
+    alert("Please enter a task");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = input.value;
+
+  const btn = document.createElement("button");
+  btn.textContent = "❌";
+
+  btn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(btn);
+  list.appendChild(li);
+
+  input.value = "";
+}
